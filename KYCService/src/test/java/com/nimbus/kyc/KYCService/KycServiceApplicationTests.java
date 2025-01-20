7540cac63a7c7d33bf409759161e85f8057a9916
juan <juan.torres.dev@gmail.com>
2025-01-19 23:28:12 -0600

    KYC Service project created

---
+++ KYCService/src/test/java/com/nimbus/kyc/KYCService/KycServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbus.kyc.KYCService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
