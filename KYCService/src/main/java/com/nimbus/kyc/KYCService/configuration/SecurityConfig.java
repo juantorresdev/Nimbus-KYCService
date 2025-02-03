e80f0540285cdb3fc0653a6690cf46fbcf5def01
juan <juan.torres.dev@gmail.com>
2025-02-02 22:06:51 -0600

    Adding paths to authorizeRequest SecurityFilterChain

--- KYCService/src/main/java/com/nimbus/kyc/KYCService/configuration/SecurityConfig.java
+++ KYCService/src/main/java/com/nimbus/kyc/KYCService/configuration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.nimbus.kyc.KYCService.configuration;
 
-import com.nimbus.kyc.KYCService.configuration.ValidationOrderFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,8 +14,9 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())  // Disable CSRF for simplicity (not recommended for production)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/kyc/**").authenticated() // Ensure all endpoints are protected
-                        .anyRequest().permitAll()
+                        .requestMatchers("/kyc/generateToken").permitAll()
+                        .requestMatchers("/kyc-phone/phone-number","/kyc-phone/otp-phone","/kyc-email/email","/kyc-email/otp-email","/kyc-persona/idpic","/kyc-persona/curp","/kyc-persona/verifyHomonimia","/kyc-address/zipcode","/kyc-address/customer-address","/kyc-address/address-location","/kyc-persona/signature","/kyc-persona/selfie")
+                        .authenticated()
                 )
                 .addFilterBefore(validationOrderFilter, UsernamePasswordAuthenticationFilter.class) // Apply custom filter
                 .build();
